988889426038cc73ce2e519c574439edcd054146
archipatel-sketch <archi.patel@webbycrown.com>
2026-03-31 13:20:07 +0530

    Update index.blade.php

--- src/resources/views/forms/index.blade.php
+++ src/resources/views/forms/index.blade.php
@@ -3,130 +3,131 @@
 @section('page-title', isset($table) ? 'List ' . formatTableName($table) : 'Records')
 
 @section('content')
-    <div class="container mt-5 p-5 border border-light rounded shadow p-3 mb-5 bg-body-tertiary">
+    <div class="container ">
 
-        <div class="shadow-sm p-2 mb-2 text-secondary rounded bg-light">
-            <h1>All {{ isset($table) ? ucfirst($table) : 'Records' }}</h1>
+        <div class="shadow p-2 mb-2 rounded bg-light mt-5 border border-light">
+            <h1 class="text-center m-3 text-dark">All {{ isset($table) ? ucfirst($table) : 'Records' }}</h1>
         </div>
 
-        <div class="mt-4 mb-4 d-flex justify-content-end">
-            <a href="{{ route('crud.create', $table) }}" class="btn btn-dark">
-                <i class="fas fa-plus-square me-2"></i> Add {{ isset($table) ? formatTableName($table) : 'Record' }}
-
-            </a>
-        </div>
-
-        @if (session('success'))
-            <div class="d-flex justify-content-center alert-msg">
-                <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    <strong>Success!</strong> {{ session('success') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
+        <div class="container mt-5 p-5 border border-light rounded shadow p-3 mb-5 bg-light">
+            <div class="mb-4 d-flex justify-content-end">
+                <a href="{{ route('crud.create', $table) }}" class="btn btn-dark">
+                    <i class="fas fa-plus-square me-2"></i> Add {{ isset($table) ? formatTableName($table) : 'Record' }}
+                </a>
             </div>
-        @endif
-        @if (session('error'))
-            <div class="d-flex justify-content-center alert-msg">
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <strong>error!</strong> {{ session('error') }}
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
-            </div>
-        @endif
 
-        <table id="dynamicTable" class="display table table-bordered mt-5">
-            <thead class="table-dark">
-                {{-- table heading --}}
-                <tr>
-                    <th>#</th>
-                    @isset($visibleColumns)
-                        @foreach ($visibleColumns as $col)
-                            <th>{{ ucfirst(str_replace('_', ' ', $col)) }}</th>
-                        @endforeach
-                    @endisset
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {{-- table data --}}
-                @foreach ($data as $index => $row)
+            @if (session('success'))
+                <div class="d-flex justify-content-center alert-msg">
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>Success!</strong> {{ session('success') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
+            @endif
+            @if (session('error'))
+                <div class="d-flex justify-content-center alert-msg">
+                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>error!</strong> {{ session('error') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                </div>
+            @endif
+
+            <table id="dynamicTable" class="display table mt-5">
+                <thead class="table-dark">
+                    {{-- table heading --}}
                     <tr>
-                        {{-- index column --}}
-                        <td>{{ $index + 1 }}</td>
-
-                        {{-- visible true columns --}}
-                        @foreach ($visibleColumns as $col)
-                            {{-- for images --}}
-                            @if (isset($images[$index]) && $col == $image['name'])
-                                <td>
-                                    @foreach ($images[$index] as $id => $path)
-                                        @if (!empty($path))
-                                            <img src="{{ asset($path) }}" alt="image" height="90px" width="90px">
-                                        @endif
-                                    @endforeach
-                                </td>
-
-                                {{-- for date  --}}
-                            @elseif (isset($date) && $col == $date['name'])
-                                <td>{{ \Carbon\Carbon::parse($row->$col)->format($date['display_formate'] ?? 'Y-m-d') }}
-                                </td>
-
-                                {{-- for checkbox --}}
-                            @elseif (isset($checkbox) && $col == $checkbox['name'])
-                                <td>
-                                    @php
-                                        $rawValue = old($col, $row->$col ?? null);
-
-                                        $values = is_string($rawValue) ? json_decode($rawValue, true) : $rawValue;
-                                    @endphp
-
-                                    @foreach ($values ?? [] as $val)
-                                        <span class="badge text-bg-secondary">{{ ucfirst($val) }}</span>
-                                    @endforeach
-                                </td>
-
-                                {{-- for toggle --}}
-                            @elseif (isset($toggle) && $col == $toggle['name'])
-                                <td>
-                                    <span class="badge {{ $row->$col ? 'bg-success' : 'bg-danger' }}">
-                                        {{ $row->$col ? 'Active' : 'Inactive' }}
-                                    </span>
-                                </td>
-                                {{-- for range --}}
-                            @elseif (isset($range) && $col == $range['name'])
-                                <td>{{ $row->$col }}</td>
-                                {{-- for color picker --}}
-                            @elseif (isset($color) && $col == $color['name'])
-                                @if (isset($color['display_formate']) && $color['display_formate']=='color')
-                                    <td>
-                                        <div
-                                            style="width: 30px; height: 30px; background-color: {{ $row->$col }}; border: 1px solid #ccc; border-radius: 4px;">
-                                        </div>
-                                    </td>
-                                @else
-                                <td>{{ $row->$col }}</td>
-                                @endif
-                            @else
-                                <td>{{ !empty($row->$col) ? ucfirst($row->$col) : '' }}</td>
-                            @endif
-                        @endforeach
-
-                        {{-- Action column --}}
-                        <td>
-                            <div class="d-flex justify-content-center">
-                                <a href="{{ route('crud.edit', ['table' => $table, 'id' => $row->id]) }}"
-                                    class="text-dark">
-                                    <i class="fas fa-edit"></i>
-                                </a>&nbsp;&nbsp;
-                                <p class="delete-record" role="button" data-table="{{ $table }}"
-                                    data-id="{{ $row->id }}">
-                                    <i class="fas fa-trash"></i>
-                                </p>
-                            </div>
-                        </td>
+                        <th>#</th>
+                        @isset($visibleColumns)
+                            @foreach ($visibleColumns as $col)
+                                <th>{{ ucfirst(str_replace('_', ' ', $col)) }}</th>
+                            @endforeach
+                        @endisset
+                        <th>Actions</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {{-- table data --}}
+                    @foreach ($data as $index => $row)
+                        <tr>
+                            {{-- index column --}}
+                            <td>{{ $index + 1 }}</td>
+
+                            {{-- visible true columns --}}
+                            @foreach ($visibleColumns as $col)
+                                {{-- for images --}}
+                                @if (isset($images[$index]) && $col == $image['name'])
+                                    <td>
+                                        @foreach ($images[$index] as $id => $path)
+                                            @if (!empty($path))
+                                                <img src="{{ asset($path) }}" alt="image" height="90px" width="90px">
+                                            @endif
+                                        @endforeach
+                                    </td>
+
+                                    {{-- for date  --}}
+                                @elseif (isset($date) && $col == $date['name'])
+                                    <td>{{ \Carbon\Carbon::parse($row->$col)->format($date['display_formate'] ?? 'Y-m-d') }}
+                                    </td>
+
+                                    {{-- for checkbox --}}
+                                @elseif (isset($checkbox) && $col == $checkbox['name'])
+                                    <td>
+                                        @php
+                                            $rawValue = old($col, $row->$col ?? null);
+
+                                            $values = is_string($rawValue) ? json_decode($rawValue, true) : $rawValue;
+                                        @endphp
+
+                                        @foreach ($values ?? [] as $val)
+                                            <span class="badge text-bg-secondary">{{ ucfirst($val) }}</span>
+                                        @endforeach
+                                    </td>
+
+                                    {{-- for toggle --}}
+                                @elseif (isset($toggle) && $col == $toggle['name'])
+                                    <td>
+                                        <span class="badge {{ $row->$col ? 'bg-success' : 'bg-danger' }}">
+                                            {{ $row->$col ? 'Active' : 'Inactive' }}
+                                        </span>
+                                    </td>
+                                    {{-- for range --}}
+                                @elseif (isset($range) && $col == $range['name'])
+                                    <td>{{ $row->$col }}</td>
+                                    {{-- for color picker --}}
+                                @elseif (isset($color) && $col == $color['name'])
+                                    @if (isset($color['display_formate']) && $color['display_formate'] == 'color')
+                                        <td>
+                                            <div
+                                                style="width: 30px; height: 30px; background-color: {{ $row->$col }}; border: 1px solid #ccc; border-radius: 4px;">
+                                            </div>
+                                        </td>
+                                    @else
+                                        <td>{{ $row->$col }}</td>
+                                    @endif
+                                @else
+                                    <td>{{ !empty($row->$col) ? ucfirst($row->$col) : '' }}</td>
+                                @endif
+                            @endforeach
+
+                            {{-- Action column --}}
+                            <td>
+                                <div class="d-flex justify-content-center">
+                                    <a href="{{ route('crud.edit', ['table' => $table, 'id' => $row->id]) }}"
+                                        class="text-dark">
+                                        <i class="fas fa-edit"></i>
+                                    </a>&nbsp;&nbsp;
+                                    <p class="delete-record" role="button" data-table="{{ $table }}"
+                                        data-id="{{ $row->id }}">
+                                        <i class="fas fa-trash"></i>
+                                    </p>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
 
