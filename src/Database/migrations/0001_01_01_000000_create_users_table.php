7ecfb06e4e69506799968dc9b0e9dddc04e189f8
archipatel-sketch <archi.patel@webbycrown.com>
2026-03-31 11:20:36 +0530

    Update 0001_01_01_000000_create_users_table.php

--- src/Database/migrations/0001_01_01_000000_create_users_table.php
+++ src/Database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,6 +22,7 @@ return new class extends Migration
             $table->string('city')->nullable();
             $table->string('image')->nullable();
             $table->boolean('status')->default(1);
+            $table->string('join_date')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
